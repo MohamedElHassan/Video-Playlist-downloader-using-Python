81fe0f264c5bba1d58a6b3cb9d94004bc0b5e148
Mohammed El Hassan <mohammedelhassan2001@gmail.com>
2021-12-03 14:46:22 +0200

    Created Directories for playlists

--- videoDownloader.py
+++ videoDownloader.py
@@ -4,25 +4,35 @@ def finish():
     print('Download Completed, Enjoy My Friend\n -----------------------------------')
 
 def downloading(resolution):
+    print('\n -----------------------------------')
     print(f'Video Quality is : {resolution}\n -----------------------------------')
-    print('Video is Downloading....\n -----------------------------------')
 
 def playListDownloading(count,res):
+    print('\n -----------------------------------')
     print(f'Video Quality is : {res}\n -----------------------------------')
-    print(f'Video Number {count} is Downloading....\n -----------------------------------')
+    print(f'Video Number {count} is Downloading....')
 
 # Download from Playlist
 ask = input('v For One Video, p For Playlist videos, n For Nothing : ')
-
 if ask == 'v':
     # --------------------------- Download Single Video
     from pytube import YouTube
     link = input('Please enter the video URL : ')
     video = YouTube(link)
-    stuff_in_string = f"C:\Users\maham\Downloads"
-    video.streams.get_lowest_resolution().download(output_path=stuff_in_string)
-    video.register_on_progress_callback(downloading(video.streams.get_lowest_resolution().resolution))
-    video.register_on_complete_callback(finish())
+    path = "C:Downloads"
+    quality = input('Please Enter the Quality you want - h for High Qualiy, l for Low Qualiy : ')
+    if quality == 'h':
+        print('Video is Downloading....\n -----------------------------------')    
+        video.streams.get_highest_resolution().download(output_path=path)
+        video.register_on_progress_callback(downloading(video.streams.get_lowest_resolution().resolution))
+        video.register_on_complete_callback(finish())
+    elif quality == 'l':
+        print('Video is Downloading....\n -----------------------------------')
+        video.streams.get_lowest_resolution().download(output_path=path)
+        video.register_on_progress_callback(downloading(video.streams.get_lowest_resolution().resolution))
+        video.register_on_complete_callback(finish())
+    else:
+                print("Don't forget to Choose the Quality You want")
 elif ask == 'p':
     # --------------------------- Download from Playlist
     from pytube import Playlist
@@ -31,17 +41,24 @@ elif ask == 'p':
     count = 2
     quality = input('Please Enter the Quality you want - h for High Qualiy, l for Low Qualiy : ')
     if quality == 'h':
+        print(f'Video Number 1 is Downloading....\n -----------------------------------')
+        dir = os.path.join("C:","Downloads",f"{playlist.title}")
+        if not os.path.exists(dir):
+            os.mkdir(dir)
         for videos in playlist.videos:
             # print(videos.streams.get_lowest_resolution())
-            videos.streams.get_highest_resolution().download(output_path='F:\A WebDevelopment\Wordpress Stuff\Wordpress Course')
-            print(f'Video Number 1 is Downloading....\n -----------------------------------')
+            videos.streams.get_highest_resolution().download(output_path='F:\A WebDevelopment\Sass Stuff')
             videos.register_on_progress_callback(playListDownloading(count,videos.streams.get_highest_resolution().resolution))
             count += 1
+            
     elif quality == 'l':
+        print(f'Video Number 1 is Downloading....\n -----------------------------------')
+        dir = os.path.join("C:","Downloads",f"{playlist.title}")
+        if not os.path.exists(dir):
+            os.mkdir(dir)
         for videos in playlist.videos:
             # print(videos.streams.get_lowest_resolution())
-            videos.streams.get_lowest_resolution().download(output_path='F:\A WebDevelopment\Wordpress Stuff\Wordpress Course')
-            print(f'Video Number 1 is Downloading....\n -----------------------------------')
+            videos.streams.get_lowest_resolution().download(output_path=f'C:Downloads\{playlist.title}')
             videos.register_on_progress_callback(playListDownloading(count,videos.streams.get_lowest_resolution().resolution))
             count += 1
     else:
